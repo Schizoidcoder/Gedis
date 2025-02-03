2ced03655e27a644f1764e825ecb35f9f69b9432
Sigma man <2200345850@qq.com>
2025-02-03 17:55:19 +0800

    readline

--- resp/parser/parser.go
+++ resp/parser/parser.go
@@ -2,6 +2,8 @@ package parser
 
 import (
 	"Gedis/interface/resp"
+	"bufio"
+	"errors"
 	"io"
 )
 
@@ -15,7 +17,7 @@ type readState struct {
 	expectedArgsCount int  //期望长度
 	msgType           byte
 	args              [][]byte
-	bulkLen           int64 //字节长度
+	bulkLen           int64 //bulkLen 存储的是批量字符串的字节长度（$len\r\n）len=bulkLen
 }
 
 func (s *readState) finished() bool {
@@ -30,4 +32,29 @@ func ParseStream(reader io.Reader) <-chan *Payload {
 
 func parse0(reader io.Reader, ch chan<- *Payload) {
 
+}
+
+func readLine(bufReader *bufio.Reader, state *readState) ([]byte, bool, error) { //第一个是读数据，第二个是有无io错误，true就是有
+	var msg []byte
+	var err error
+	if state.bulkLen == 0 { //1.\r\n切分
+		msg, err = bufReader.ReadBytes('\n')
+		if err != nil { // io错误
+			return nil, true, err
+		}
+		if len(msg) == 0 || msg[len(msg)-2] != '\r' {
+			return nil, false, errors.New("protocol error:" + string(msg))
+		}
+	} else { //2.之前读到了$数字，严格读取字符个数
+		msg = make([]byte, state.bulkLen+2) //还要把\r\n读进来
+		_, err = io.ReadFull(bufReader, msg)
+		if err != nil {
+			return nil, true, err
+		}
+		if len(msg) == 0 || msg[len(msg)-2] != '\r' || msg[len(msg)-1] != '\n' {
+			return nil, false, errors.New("protocol error:" + string(msg))
+		}
+		state.bulkLen = 0
+	}
+	return msg, false, nil
 }
