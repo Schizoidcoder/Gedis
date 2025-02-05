436ed8f429010cfdf4f20434ee04c538e2790158
Sigma man <2200345850@qq.com>
2025-02-05 15:05:59 +0800

    SyncDict

--- datastruct/dict/dict.go
+++ datastruct/dict/dict.go
@@ -4,7 +4,7 @@ type Consumer func(key string, val interface{}) bool
 
 type Dict interface {
 	Get(key string) (val interface{}, exists bool)
-	Len()
+	Len() int
 	Put(key string, val interface{}) (result int)
 	PutIfAbsent(key string, val interface{}) (result int)
 	PutIfExists(key string, val interface{}) (result int)
---
+++ datastruct/dict/sync_dict.go
@@ -0,0 +1,106 @@
+package dict
+
+import "sync"
+
+type SyncDict struct {
+	m sync.Map
+}
+
+func MakeSyncDict() *SyncDict {
+	return &SyncDict{}
+}
+
+func (dict *SyncDict) Get(key string) (val interface{}, exists bool) {
+	val, ok := dict.m.Load(key)
+	return val, ok
+}
+
+func (dict *SyncDict) Len() int {
+	lenth := 0
+	dict.m.Range(func(key, value interface{}) bool {
+		lenth++
+		return true
+	})
+	return lenth
+}
+
+func (dict *SyncDict) Put(key string, val interface{}) (result int) {
+	_, existed := dict.m.Load(key)
+	dict.m.Store(key, val)
+	if existed {
+		return 0 //修改了或没插入
+	}
+	return 1 //插入新的
+}
+
+func (dict *SyncDict) PutIfAbsent(key string, val interface{}) (result int) {
+	_, existed := dict.m.Load(key)
+	if existed {
+		return 0
+	}
+	return 1
+}
+
+func (dict *SyncDict) PutIfExists(key string, val interface{}) (result int) {
+	_, existed := dict.m.Load(key)
+	if existed {
+		return 1
+	}
+	return 0
+}
+
+func (dict *SyncDict) Remove(key string) (result int) {
+	_, existed := dict.m.Load(key)
+	dict.m.Delete(key)
+	if existed {
+		return 1
+	}
+	return 0
+}
+
+func (dict *SyncDict) ForEach(consumer Consumer) {
+	dict.m.Range(func(key, value interface{}) bool {
+		consumer(key.(string), value)
+		return true
+	})
+}
+
+func (dict *SyncDict) Keys() []string {
+	result := make([]string, 0, dict.Len())
+	i := 0
+	dict.m.Range(func(key, value interface{}) bool {
+		result[i] = key.(string)
+		i++
+		return true
+	})
+	return result
+}
+
+func (dict *SyncDict) RandomKeys(limit int) []string {
+	result := make([]string, 0, limit)
+	for i := 0; i < limit; i++ {
+		dict.m.Range(func(key, value interface{}) bool {
+			result[i] = key.(string)
+			return false
+		})
+	}
+	return result
+}
+
+func (dict *SyncDict) RandomDistinctKeys(limit int) []string {
+	result := make([]string, 0, limit)
+	i := 0
+	dict.m.Range(func(key, value interface{}) bool {
+		result[i] = key.(string)
+		i++
+		if i == limit {
+			return false
+		}
+		return true
+	})
+	return result
+}
+
+func (dict *SyncDict) clear() {
+	*dict = *MakeSyncDict() // 旧地址让系统垃圾回收
+}
