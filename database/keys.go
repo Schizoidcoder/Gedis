c1b31e46085e1b505159248d5ed579ef9897a01c
Sigma man <2200345850@qq.com>
2025-02-06 14:23:02 +0800

    keys

---
+++ database/keys.go
@@ -0,0 +1,103 @@
+package database
+
+import (
+	"Gedis/interface/resp"
+	"Gedis/lib/wildcard"
+	"Gedis/resp/reply"
+)
+
+// DEL k1 k2 k3
+func execDel(db *DB, args [][]byte) resp.Reply {
+	keys := make([]string, len(args))
+	for i, v := range args {
+		keys[i] = string(v)
+	}
+	deleted := db.Removes(keys...)
+	return reply.MakeIntReply(int64(deleted))
+}
+
+// EXISTS k1 k2 k3
+func execExists(db *DB, args [][]byte) resp.Reply {
+	result := int64(0)
+	for _, arg := range args {
+		key := string(arg)
+		_, exists := db.GetEntity(key)
+		if exists {
+			result++
+		}
+	}
+	return reply.MakeIntReply(result)
+}
+
+// FLUSH DB
+func execFlushDB(db *DB, args [][]byte) resp.Reply {
+	db.Flush()
+	return reply.MakeOkReply()
+}
+
+// TYPE k1 获取v的类型
+func execType(db *DB, args [][]byte) resp.Reply {
+	key := string(args[0])
+	entity, exists := db.GetEntity(key)
+	if !exists {
+		return reply.MakeStatusReply("none")
+	}
+	switch entity.Data.(type) {
+	case []byte:
+		return reply.MakeStatusReply("string")
+	}
+	return reply.UnknownErrReply{}
+}
+
+//RENAME 修改key v不变 RENAME k1 k2 如果k2存在 就会被覆盖
+func execRename(db *DB, args [][]byte) resp.Reply {
+	src := string(args[0])
+	dest := string(args[1])
+	entity, exists := db.GetEntity(src)
+	if !exists {
+		return reply.MakeErrReply("no such key")
+	}
+	db.Remove(src)
+	db.PutEntity(dest, entity)
+	return reply.MakeOkReply()
+}
+
+//RENAME NX k1 k2
+func execRenamenx(db *DB, args [][]byte) resp.Reply {
+	src := string(args[0])
+	dest := string(args[1])
+	_, exists := db.GetEntity(dest)
+	if !exists {
+		return reply.MakeIntReply(0)
+	}
+	entity, exists := db.GetEntity(src)
+	if !exists {
+		return reply.MakeErrReply("no such key")
+	}
+	db.Remove(src)
+	db.PutEntity(dest, entity)
+	return reply.MakeIntReply(1)
+}
+
+//KEYS * 列出db里面所有的key
+func execKeys(db *DB, args [][]byte) resp.Reply {
+	pattern := wildcard.CompilePattern(string(args[0]))
+	result := make([][]byte, 0)
+	db.data.ForEach(func(key string, val interface{}) bool {
+		if pattern.IsMatch(key) {
+			result = append(result, []byte(key))
+		}
+		return true
+	})
+	return reply.MakeMultiBulkReply(result)
+}
+
+func init() {
+	RegisterCommand("DEL", execDel, -2)
+	RegisterCommand("EXISTS", execExists, -2)
+	RegisterCommand("FLUSHDB", execFlushDB, 1)
+	RegisterCommand("TYPE", execType, 2)
+	RegisterCommand("RENAME", execRename, 3)
+	RegisterCommand("RENAMENX", execRenamenx, 3)
+	RegisterCommand("KEYS", execKeys, 2)
+}
