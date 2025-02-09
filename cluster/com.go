519640524c80891bff144bd4c65e3f53c86702f4
Sigma man <2200345850@qq.com>
2025-02-10 00:49:28 +0800

    relay&broadcast

--- cluster/com.go
+++ cluster/com.go
@@ -1,9 +1,13 @@
 package cluster
 
 import (
+	"Gedis/interface/resp"
+	"Gedis/lib/utils"
 	"Gedis/resp/client"
+	"Gedis/resp/reply"
 	"context"
 	"errors"
+	"strconv"
 )
 
 func (cluster *ClusterDatabase) getPeerClient(peer string) (*client.Client, error) {
@@ -29,4 +33,28 @@ func (cluster *ClusterDatabase) returnPeerClient(peer string, peerClient *client
 	}
 	return pool.ReturnObject(context.Background(), peerClient)
 
+}
+
+func (cluster *ClusterDatabase) relay(peer string, c resp.Connection, args [][]byte) resp.Reply {
+	if peer == cluster.self {
+		return cluster.db.Exec(c, args)
+	}
+	peerClient, err := cluster.getPeerClient(peer)
+	if err != nil {
+		return reply.MakeErrReply(err.Error())
+	}
+	defer func() {
+		_ = cluster.returnPeerClient(peer, peerClient)
+	}()
+	peerClient.Send(utils.ToCmdLine("SELECT", strconv.Itoa(c.GetDBIndex())))
+	return peerClient.Send(args)
+}
+
+func (cluster *ClusterDatabase) broadcast(c resp.Connection, args [][]byte) map[string]resp.Reply {
+	results := make(map[string]resp.Reply)
+	for _, node := range cluster.nodes {
+		result := cluster.relay(node, c, args)
+		results[node] = result
+	}
+	return results
 }
